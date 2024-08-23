a8b66c610abcea96c496cf8699b33bb5e7ffb679
Jay Mercado <jay.gutierrezmercado@gmail.com>
2024-08-24 07:48:34 +0800

    Fix initial loading of home page

--- src/app/(views)/layout.tsx
+++ src/app/(views)/layout.tsx
@@ -11,20 +11,22 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
 
   if (!data) signIn()
 
-  return (
-    <>
-      <AppSidebar />
-      <div className="wrapper d-flex flex-column min-vh-100">
-        <AppHeader />
-        <div className="body flex-grow-1">
-          <CContainer lg className="px-4">
-            <AppBreadcrumb />
-            {children}
-          </CContainer>
+  if (data) {
+    return (
+      <>
+        <AppSidebar />
+        <div className="wrapper d-flex flex-column min-vh-100">
+          <AppHeader />
+          <div className="body flex-grow-1">
+            <CContainer lg className="px-4">
+              <AppBreadcrumb />
+              {children}
+            </CContainer>
+          </div>
+          <AppFooter />
         </div>
-        <AppFooter />
-      </div>
-      <AppAside />
-    </>
-  )
+        <AppAside />
+      </>
+    )
+  }
 }
