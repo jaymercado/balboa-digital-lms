95b8b5dce6c9c5445ce290760a95f11a06f03641
Jay Mercado <jay.gutierrezmercado@gmail.com>
2024-09-23 15:46:36 +0800

    Rename courseType back to course

--- src/hooks/useGetCourses.ts
+++ src/hooks/useGetCourses.ts
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Course as CourseType } from '@/types/course'
+import { Course as CourseType } from '@/types/courseType'
 
 export default function useGetCourses({
   type,
