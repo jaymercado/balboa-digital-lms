82a71a134954993d0ae3821e5ef00dc1c6fb72bb
Jay Mercado <jay.gutierrezmercado@gmail.com>
2024-09-30 20:42:38 +0800

    Fix authOptions

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,10 +3,11 @@ import { NextRequest, NextResponse } from 'next/server'
 import connectMongo from '@/utils/mongodb'
 import UserModel from '@/models/User'
 import { getServerSession } from 'next-auth'
+import { authOptions } from '../auth/[...nextauth]/route'
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   try {
-    const session = await getServerSession()
+    const session = await getServerSession(authOptions)
 
     if (!session?.user.email) {
       return NextResponse.json({ error: 'Session not found' }, { status: 400 })
