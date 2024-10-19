dd12a268131c797c7cfae9a7fb40f0b67aba5396
Jay Mercado <jay.gutierrezmercado@gmail.com>
2024-10-19 10:13:33 +0800

    Created Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  )
+}
