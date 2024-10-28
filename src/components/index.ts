67b33892b5a67d24c2715a4754c0ef20c88c1360
Jay Mercado <jay.gutierrezmercado@gmail.com>
2024-10-28 11:57:00 +0800

    LandingPage

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ import AppHeader from './AppHeader'
 import AppSidebar from './AppSidebar'
 import CreateLink from './CreateLink'
 import Nav from './Nav'
+import LandingPage from './LandingPage'
 import Loading from './Loading'
 
-export { AppBreadcrumb, AppFooter, AppHeader, AppSidebar, CreateLink, Nav, Loading }
+export { AppBreadcrumb, AppFooter, AppHeader, AppSidebar, CreateLink, Nav, LandingPage, Loading }
