509ef9b5cacb8ba9ab0aedb2115a73c6ceca416d
LloydFrancisco <lloyddaenielfrancisco@gmail.com>
2024-11-14 17:47:22 +0800

    Fixed inline texting of Loading in Enrolled Courses

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,14 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export default function Loading() {
   return (
-    <div 
-      className="flex justify-center items-center h-screen" 
-      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '60vh' }}
-    >
+    <div className="loading-container">
       <div className="spinner-border" role="status">
         <span className="visually-hidden">Loading...</span>
       </div>
     </div>
-  )
+  );
 }
