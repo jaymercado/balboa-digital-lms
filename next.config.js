a4ae707cde28ba7cde21813776db9c0ffab4c4b7
Jay Mercado <jay.gutierrezmercado@gmail.com>
2024-11-20 18:50:20 +0800

    pdf

--- next.config.js
+++ next.config.js
@@ -14,6 +14,13 @@ const nextConfig = {
   },
   webpack: (config) => {
     config.resolve.alias.canvas = false
+
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    })
+
     return config
   },
 }
