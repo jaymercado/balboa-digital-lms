e3965db80a37fc6eab3bc9de74a00c439a63c1d7
Alexander <ajoxales@gmail.com>
2024-11-28 20:58:44 +0800

    [BAL-16] Build UI For Enrolled Course

--- src/app/(views)/enrolled-courses/[courseId]/modules/page.tsx
+++ src/app/(views)/enrolled-courses/[courseId]/modules/page.tsx
@@ -5,11 +5,22 @@ import Link from 'next/link'
 import { useParams } from 'next/navigation'
 import useGetCourses from '@/hooks/useGetCourses'
 import { Loading } from '@/components'
+import {
+  CTableHead,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHeaderCell,
+  CTableRow,
+  CCard,
+  CCardBody,
+} from '@coreui/react-pro'
+import CIcon from '@coreui/icons-react'
+import { cilVideo, cilImage, cilFile, cilNotes } from '@coreui/icons'
 
 export default function Modules() {
   const params = useParams()
   const { courseId } = params as { courseId: string }
-
   const { courses, fetchingCourses } = useGetCourses({ courseId })
 
   if (fetchingCourses) {
@@ -17,29 +28,63 @@ export default function Modules() {
   }
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>Type</th>
-          <th>Content</th>
-        </tr>
-      </thead>
-      <tbody>
-        {courses[0]?.modules.map((module) => (
-          <tr key={module.id}>
-            <td>
-              <Link href={`/enrolled-courses/${courses[0]?.id}/modules/${module.id}`}>
-                {module.id}
-              </Link>
-            </td>
-            <td>{module.title}</td>
-            <td>{module.type}</td>
-            <td dangerouslySetInnerHTML={{ __html: module.content }} />
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <CCard>
+      <CCardBody>
+        <CTable striped>
+          <CTableHead>
+            <CTableRow className="text-secondary">
+              <CTableHeaderCell>
+                <small>ID</small>
+              </CTableHeaderCell>
+              <CTableHeaderCell>
+                <small>Title</small>
+              </CTableHeaderCell>
+              <CTableHeaderCell>
+                <small>Type</small>
+              </CTableHeaderCell>
+            </CTableRow>
+          </CTableHead>
+          <CTableBody>
+            {courses[0]?.modules.length > 0 ? (
+              courses[0]?.modules.map((module) => (
+                <CTableRow key={module.id} align="middle">
+                  <CTableDataCell>
+                    <Link href={`/enrolled-courses/${courses[0]?.id}/modules/${module.id}`}>
+                      {module.id}
+                    </Link>
+                  </CTableDataCell>
+                  <CTableDataCell>
+                    <Link
+                      href={`/enrolled-courses/${courses[0]?.id}/modules/${module.id}`}
+                      className="text-decoration-none"
+                    >
+                      <span className="fw-semibold">{module.title}</span>
+                      <small className="d-block text-truncate text-secondary description">
+                        {module.description}
+                      </small>
+                    </Link>
+                  </CTableDataCell>
+                  <CTableDataCell>
+                    {module.type === 'video' && <CIcon icon={cilVideo} size="sm" color="dark" />}
+                    {module.type === 'text' && <CIcon icon={cilNotes} size="sm" color="dark" />}
+                    {module.type === 'pdf' && <CIcon icon={cilFile} size="sm" color="dark" />}
+                    {module.type === 'image' && <CIcon icon={cilImage} size="sm" color="dark" />}
+                    <small className="text-secondary ms-1">
+                      <span className="text-capitalize">{module.type}</span>
+                    </small>
+                  </CTableDataCell>
+                </CTableRow>
+              ))
+            ) : (
+              <CTableRow>
+                <CTableDataCell colSpan={4} className="text-center">
+                  No modules available
+                </CTableDataCell>
+              </CTableRow>
+            )}
+          </CTableBody>
+        </CTable>
+      </CCardBody>
+    </CCard>
   )
 }
