c223f56b1b4b02860272cff443b5efa16ee89d62
Alexander <ajoxales@gmail.com>
2025-02-04 19:19:13 +0800

    [BAL-62] Add Chart on Dashboard

--- src/components/InstructorDashboard.tsx
+++ src/components/InstructorDashboard.tsx
@@ -1,7 +1,6 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Link from 'next/link'
-import CIcon from '@coreui/icons-react'
 import {
   CRow,
   CCol,
@@ -10,32 +9,91 @@ import {
   CCardTitle,
   CTable,
   CBadge,
-  CAvatar,
+  CTableHeaderCell,
+  CTableRow,
+  CTableHead,
+  CTableBody,
+  CTableDataCell,
 } from '@coreui/react-pro'
+import CIcon from '@coreui/icons-react'
 import { cilEducation, cilBadge, cilBook, cilPencil } from '@coreui/icons'
+import { CChart } from '@coreui/react-chartjs'
+import { getStyle } from '@coreui/utils'
+import type { ChartData } from 'chart.js'
 import { useGetCourses } from '@/hooks/useGetCourses'
-import Loading from './Loading'
 import { useGetAllUserCourseItemLogs } from '@/hooks/useGetAllUserCourseItemLogs'
+import Loading from './Loading'
 
 export default function InstructorDashboard() {
   const { courses, setCourses, fetchingCourses } = useGetCourses({ type: 'managed' })
   const { coursesWithCompletionStatus, fetchingUserCourseItemLogs } = useGetAllUserCourseItemLogs()
-  const completedCourses = coursesWithCompletionStatus.filter((course) => course.completed)
+
+  const notStartedCourses = coursesWithCompletionStatus.filter(
+    (course) => course.status === 'notStarted',
+  )
+  const completedCourses = coursesWithCompletionStatus.filter(
+    (course) => course.status === 'completed',
+  )
   const inProgressCourses = coursesWithCompletionStatus.filter(
-    (course) => course.completed === false,
+    (course) => course.status === 'inProgress',
   )
 
+  const chartRef = useRef<any>(null)
+
+  useEffect(() => {
+    const handleColorSchemeChange = () => {
+      const chartInstance = chartRef.current
+      if (chartInstance) {
+        const { options } = chartInstance
+
+        if (options.plugins?.legend?.labels) {
+          options.plugins.legend.labels.color = getStyle('--cui-body-color')
+        }
+
+        chartInstance.update()
+      }
+    }
+
+    document.documentElement.addEventListener('ColorSchemeChange', handleColorSchemeChange)
+
+    return () => {
+      document.documentElement.removeEventListener('ColorSchemeChange', handleColorSchemeChange)
+    }
+  }, [])
+
+  const data: ChartData<'doughnut'> = {
+    labels: ['Managed', 'Enrolled', 'Completed', 'In Progress', 'Not Started'],
+    datasets: [
+      {
+        backgroundColor: [
+          getStyle('--cui-info'),
+          getStyle('--cui-primary'),
+          getStyle('--cui-success'),
+          getStyle('--cui-warning'),
+          getStyle('--cui-danger'),
+        ],
+        data: [
+          courses?.length,
+          coursesWithCompletionStatus?.length,
+          inProgressCourses?.length,
+          inProgressCourses?.length,
+          notStartedCourses?.length,
+        ],
+      },
+    ],
+  }
+
   return (
-    <div>
+    <div className="mb-3">
       <CRow>
         <CCol className="mb-3">
-          <CCard className="p-3 bg-danger-subtle text-danger-emphasis d-flex justify-content-between flex-row">
-            <div className="border-start border-start-4 border-start-danger py-1 px-3">
+          <CCard className="p-3 bg-info-subtle text-info-emphasis d-flex justify-content-between flex-row">
+            <div className="border-start border-start-4 border-start-info py-1 px-3">
               <div className="text-secondary text-truncate small">Managed Courses</div>
               <div className="fs-4 fw-semibold">{courses?.length}</div>
             </div>
-            <div className="bg-danger bg-opacity-25 rounded p-3 d-flex align-items-center">
-              <CIcon icon={cilPencil} color="danger" size="xl" />
+            <div className="bg-info bg-opacity-25 rounded p-3 d-flex align-items-center">
+              <CIcon icon={cilPencil} color="info" size="xl" />
             </div>
           </CCard>
         </CCol>
@@ -72,6 +130,17 @@ export default function InstructorDashboard() {
             </div>
           </CCard>
         </CCol>
+        <CCol className="mb-3">
+          <CCard className="p-3 bg-danger-subtle text-danger-emphasis d-flex justify-content-between flex-row">
+            <div className="border-start border-start-4 border-start-danger py-1 px-3">
+              <div className="text-secondary text-truncate small">Not Started</div>
+              <div className="fs-4 fw-semibold">{notStartedCourses?.length}</div>
+            </div>
+            <div className="bg-danger bg-opacity-25 rounded p-3 d-flex align-items-center">
+              <CIcon icon={cilBook} color="danger" size="xl" />
+            </div>
+          </CCard>
+        </CCol>
       </CRow>
       <CRow className="mb-3">
         <CCol>
@@ -84,9 +153,9 @@ export default function InstructorDashboard() {
                 </Link>
               </CCardTitle>
               {fetchingCourses ? (
-                <tr>
+                <div className="d-flex justify-content-center align-items-center">
                   <Loading />
-                </tr>
+                </div>
               ) : (
                 <CTable hover responsive>
                   <thead>
@@ -130,6 +199,30 @@ export default function InstructorDashboard() {
             </CCardBody>
           </CCard>
         </CCol>
+        <CCol xs={4}>
+          <CCard className="h-100">
+            <CCardBody>
+              <CCardTitle className="fw-semibold">Course Status</CCardTitle>
+              <CChart
+                type="doughnut"
+                data={data}
+                options={{
+                  responsive: true,
+                  aspectRatio: 1,
+                  plugins: {
+                    legend: {
+                      position: 'bottom',
+                    },
+                    tooltip: {
+                      enabled: true,
+                    },
+                  },
+                }}
+                ref={chartRef}
+              />
+            </CCardBody>
+          </CCard>
+        </CCol>
       </CRow>
       <CRow>
         <CCol>
@@ -142,40 +235,40 @@ export default function InstructorDashboard() {
                 </Link>
               </CCardTitle>
               {fetchingUserCourseItemLogs ? (
-                <tr>
+                <div className="d-flex justify-content-center align-items-center">
                   <Loading />
-                </tr>
+                </div>
               ) : (
                 <CTable hover responsive>
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Course Title</th>
-                      <th>Status</th>
-                    </tr>
-                  </thead>
-                  <tbody>
+                  <CTableHead>
+                    <CTableRow>
+                      <CTableHeaderCell>ID</CTableHeaderCell>
+                      <CTableHeaderCell>Course Title</CTableHeaderCell>
+                      <CTableHeaderCell>Status</CTableHeaderCell>
+                    </CTableRow>
+                  </CTableHead>
+                  <CTableBody>
                     {coursesWithCompletionStatus.length > 0 ? (
                       coursesWithCompletionStatus.slice(0, 5).map((course) => (
-                        <tr key={course.id}>
-                          <td>
+                        <CTableRow key={course.id}>
+                          <CTableDataCell>
                             <Link
                               href={`/enrolled-courses/${course.id}`}
                               className="text-decoration-none"
                             >
                               {course.id}
                             </Link>
-                          </td>
-                          <td>
+                          </CTableDataCell>
+                          <CTableDataCell>
                             <Link
                               href={`/enrolled-courses/${course.id}`}
                               className="text-decoration-none"
                             >
                               {course.title}
                             </Link>
-                          </td>
-                          <td>
-                            {course.completed ? (
+                          </CTableDataCell>
+                          <CTableDataCell>
+                            {course.status === 'completed' ? (
                               <CBadge
                                 color="success"
                                 shape="rounded-pill"
@@ -183,25 +276,32 @@ export default function InstructorDashboard() {
                               >
                                 Completed
                               </CBadge>
-                            ) : (
+                            ) : course.status === 'inProgress' ? (
                               <CBadge
                                 shape="rounded-pill"
                                 className="text-warning-emphasis bg-warning-subtle"
                               >
                                 In Progress
                               </CBadge>
+                            ) : (
+                              <CBadge
+                                shape="rounded-pill"
+                                className="text-danger-emphasis bg-danger-subtle"
+                              >
+                                Not Started
+                              </CBadge>
                             )}
-                          </td>
-                        </tr>
+                          </CTableDataCell>
+                        </CTableRow>
                       ))
                     ) : (
-                      <tr>
-                        <td colSpan={3} className="text-center fw-bold">
+                      <CTableRow>
+                        <CTableDataCell colSpan={3} className="text-center fw-bold">
                           No courses found
-                        </td>
-                      </tr>
+                        </CTableDataCell>
+                      </CTableRow>
                     )}
-                  </tbody>
+                  </CTableBody>
                 </CTable>
               )}
             </CCardBody>
